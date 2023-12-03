653a60e4ba551464cd8b442c371e1d3f6aeb38bd
IvanMi95 <117540672+IvanMi95@users.noreply.github.com>
2023-12-03 21:12:00 +0100

    feat: project setup

--- dist/db/schema/users.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pg_core_1 = require("drizzle-orm/pg-core");
-const users = (0, pg_core_1.pgTable)("users", {
-    id: (0, pg_core_1.serial)("id").primaryKey(),
-    name: (0, pg_core_1.text)("name"),
-    surname: (0, pg_core_1.text)("surname")
-});
