37ab391f08229a8dc27060fc3cb728aaead05c36
IvanMi95 <117540672+IvanMi95@users.noreply.github.com>
2023-12-03 13:56:09 +0100

    feat: set up db

--- db/schema.ts
+++ db/schema.ts
@@ -1,13 +1,29 @@
-import {  pgTable, serial, text } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-export const users = pgTable("users",{
-    id: serial("id").primaryKey(),
-    name:text("name"),
-    surname:text("surname")
-})
+export const company = pgTable("company", {
+  id: serial("company_id").primaryKey(),
+  name: varchar("name", { length: 256 }),
+});
+export const users = pgTable("users", {
+  email: varchar("email", { length: 256 }).primaryKey(),
+  companyId: serial("company_id").references(() => company.id),
+  name: varchar("name", { length: 256 }),
+  surname: varchar("surname", { length: 256 }),
+  passwordHash: varchar("passwordHash", { length: 256 }),
+  lastLogin: timestamp("last_login").defaultNow(),
+});
 
-export const repport = pgTable("repport",{
-    id: serial("id").primaryKey(),
-    title:text("name"),
-    hours:text("surname")
-})
+export const repport = pgTable("repport", {
+  reportId: serial("report_id").primaryKey(),
+  title: varchar("title", { length: 256 }),
+  content: text("content"),
+  minutes: integer("minutes"),
+  submissionDate: timestamp("submission_date").defaultNow(),
+});
